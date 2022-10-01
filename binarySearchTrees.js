3991fda4e5ed70c80db84ea0ee33427af1b6bdcb
shivam singh <shivamsingh97166@gmail.com>
2022-10-01 22:36:08 +0530

    traversing tree completed

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -71,16 +71,80 @@ class BinarySearchTree {
         }
         return false;
     }
+
+    BFS() {
+        let node = this.root,
+            data = [],
+            queue = [];
+        queue.push(node);
+
+        while (queue.length) {
+            node = queue.shift()
+            // data.push(node);
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
+
+    DFSPreOrder() {
+        let data = [];
+        function traverse(node) {
+            data.push(node.value);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    DFSPostOrder() {
+        let data = [];
+        function traverse(node) {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    DFSInOrder() {
+        let data = [];
+        function traverse(node) {
+            // if (node.left) traverse(node.left);
+            // data.push(node.value);
+            // if (node.right) traverse(node.right);
+            // or 
+            node.left && traverse(node.left);
+            data.push(node.value);
+            node.right && traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
 
 let tree = new BinarySearchTree();
-tree.insert(10);
-tree.insert(5);
-tree.insert(2);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-tree.insert(7);
+// tree.insert(10);
+// tree.insert(5);
+// tree.insert(2);
+// tree.insert(13);
+// tree.insert(11);
+// tree.insert(2);
+// tree.insert(16);
+// tree.insert(7);
 // console.log(tree.find(500));
-console.log(tree.contains(500));
+// console.log(tree.contains(500));
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+// console.log(tree.BFS());
+console.log(tree.DFSPreOrder());
+console.log(tree.DFSPostOrder());
+console.log(tree.DFSInOrder());
+
